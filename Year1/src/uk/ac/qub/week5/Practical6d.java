9355623a3659d8b677de877e85b32626699dcfe4
Gerald Craig <geraldcraig@Geralds-Mac-mini.local>
2020-10-22 21:01:17 +0100

    exercises added

---
+++ Year1/src/uk/ac/qub/week5/Practical6d.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.ac.qub.week5;
+
+/**
+ * @author geraldcraig
+ *
+ */
+public class Practical6d {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
