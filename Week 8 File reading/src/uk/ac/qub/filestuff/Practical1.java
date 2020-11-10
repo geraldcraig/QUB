136a8804f5832f9e95d4fc3a7308e72c419c07fd
Gerald Craig <geraldcraig@me.com>
2020-11-10 08:05:44 +0000

    Week 8 part a completed

--- Week 8 File reading/src/uk/ac/qub/filestuff/Practical1.java
+++ Week 8 File reading/src/uk/ac/qub/filestuff/Practical1.java
@@ -19,37 +19,41 @@ public class Practical1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		// get access to a file
-				File file = new File("Catch 22.txt");
-				String line;
-				
-				// read the file
-				try {
-					FileReader fr = new FileReader(file);
-					BufferedReader br = new BufferedReader(fr);
-					
-					line = br.readLine();
+		File file = new File("Catch 22.txt");
+		String line;
+		int count = 0;
 
-					// output to screen
-					while (line != null) {
-						System.out.println(line);
-						line = br.readLine();			}
-					
+		// read the file
+		try {
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
 
-					// end of file
+			line = br.readLine();
 
-					// close any resources
-					br.close();
-					fr.close();
+			// output to screen
+			while (line != null) {
+				System.out.println(line);
+				count++;
+				line = br.readLine();
+			}
+			
+			System.out.println("The number of lines is : " + count);
 
-				} catch (FileNotFoundException e) {
-					System.out.println("File not found");
-				} catch (IOException e) {
-					System.out.println("Problem with reading the file");
-				} catch (Exception e) {
-					System.out.println("Had a problem...");
-				}
+			// end of file
+
+			// close any resources
+			br.close();
+			fr.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Problem with reading the file");
+		} catch (Exception e) {
+			System.out.println("Had a problem...");
+		}
 
 	}
 
