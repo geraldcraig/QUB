90fd7ba27c422b6be0e07b38f33f2e61ea9e8e47
Gerald Craig <geraldcraig@geralds-mini.lan>
2020-11-19 15:34:10 +0000

    test file added

---
+++ Hello World/src/uk/ac/qub/test/Hello.java
@@ -0,0 +1,5 @@
+package uk.ac.qub.test;
+
+public class Hello {
+    System.out.println("Hello World");
+}
---
+++ Test/src/ac/qub/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ac.qub;
+
+/**
+ * @author geraldcraig
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
