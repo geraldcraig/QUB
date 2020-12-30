0f649646f513cb38280ec94a3c4f25e66f41fa00
Gerald Craig <geraldcraig@me.com>
2020-12-30 08:03:55 +0000

    week 10 practical added

---
+++ Week 10 Intro To OOP/src/uk/ac/qub/practical1/Quotes.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package uk.ac.qub.practical1;
+
+/**
+ * Class that has several Literary Quotes methods
+ * @author geraldcraig
+ *
+ */
+public class Quotes {
+	/**
+	 * Outputs Shakespeare quote to screen
+	 */
+	public void shakespeareQuote() {
+		System.out.println("Good night, good night! Parting is such sweet sorrow, that I shall say good night till it be morrow. Shakespeare");
+	}
+	
+	/**
+	 * Outputs Shakespeare quote to screen
+	 */
+	public void oscarWildeQuote() {
+		System.out.println("Experience is simply the name we give our mistakes. Wilde");
+	}
+
+	/**
+	 * Outputs Churchill quote to screen
+	 */
+	public void churchillQuote() {
+		System.out.println("Continuous effort - not strength or intelligence - is the key to unlocking our potential. Churchill");
+	}
+	
+	/**
+	 * Outputs George W Bush quote to screen
+	 */
+	public void georgeWBushQuote() {
+		System.out.println("One of the great things about books is sometimes there are some fantastic pictures. George W Bush");
+	}
+	
+	/**
+	 * Outputs Einstein quote to screen
+	 */
+	public void albertEinstein() {
+		System.out.println("If the facts don't fit the theory, change the facts. Albert Einstein");
+	}
+}
