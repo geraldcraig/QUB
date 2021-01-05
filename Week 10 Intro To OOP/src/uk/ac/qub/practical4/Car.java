46c060ed1f9491d2051102a38b0f64055655e473
Gerald Craig <geraldcraig@me.com>
2021-01-05 22:37:56 +0000

    week 10 updated

--- Week 10 Intro To OOP/src/uk/ac/qub/practical4/Car.java
+++ Week 10 Intro To OOP/src/uk/ac/qub/practical4/Car.java
@@ -17,11 +17,80 @@ public class Car {
 	private int maxSpeed;
 	private boolean started;
 	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public int getNumberOfDoors() {
+		return numberOfDoors;
+	}
+
+	public void setNumberOfDoors(int numberOfDoors) {
+		this.numberOfDoors = numberOfDoors;
+	}
+
+	public double getEngineSize() {
+		return engineSize;
+	}
+
+	public void setEngineSize(double engineSize) {
+		this.engineSize = engineSize;
+	}
+
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public boolean isStarted() {
+		return started;
+	}
+
+	public void setStarted(boolean started) {
+		this.started = started;
+	}
+
 	public void startCar() {
+		if (this.started) {
+			System.out.println("Engine already running");
+		} else {
+			this.started = true;
+			System.out.println("Engine started");
+		}
 		
 	}
 	
 	public void stopCar() {
+		if (!this.started) {
+			System.out.println("Engine already stopped");
+		} else {
+			this.started = false;
+			System.out.println("Engine stopped");
+		}
 		
 	}
 
--- Week 10 Intro To OOP/src/uk/ac/qub/practical4/CarApp.java
+++ Week 10 Intro To OOP/src/uk/ac/qub/practical4/CarApp.java
@@ -14,9 +14,28 @@ public class CarApp {
 	 */
 	public static void main(String[] args) {
 		
-		Car make = new Car();
+		Car car1 = new Car();
+		car1.setName("Ford");
+		car1.setModel("Fiesta");
+		car1.setColour("Blue");
+		car1.setNumberOfDoors(4);
+		car1.setEngineSize(1.2);
+		car1.setMaxSpeed(110);
 		
+		car1.startCar();
+		car1.startCar();
+		car1.stopCar();
+		car1.stopCar();
 		
+		Car car2 = new Car();
+		car1.setName("Ford");
+		car1.setModel("Fiesta");
+		car1.setColour("Blue");
+		car1.setNumberOfDoors(4);
+		car1.setEngineSize(1.2);
+		car1.setMaxSpeed(110);
+		
+		System.out.println(car1.toString());
 
 	}
 
