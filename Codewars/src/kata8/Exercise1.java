33379b905de3088673759c34fef9f3475a51fde1
Gerald Craig <geraldcraig@me.com>
2021-01-28 19:55:16 +0000

    exercises updated

---
+++ Codewars/src/kata8/Exercise1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package kata8;
+
+/**
+ * @author Gerald
+ *
+ */
+public class Exercise1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Coding Bat/src/warm/up/Warm.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package warm.up;
+
+/**
+ * @author Gerald
+ *
+ */
+public class Warm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
