a9bf5dfc883d984fd7ac97b2e1cdc8aae5eafff9
Gerald Craig <geraldcraig@me.com>
2021-02-03 20:38:06 +0000

    book challenge added

---
+++ Week 07 Assessment/src/uk/ac/qub/assessment1/WordSortChallenge.java
@@ -0,0 +1,21 @@
+package uk.ac.qub.assessment1;
+
+public class WordSortChallenge {
+
+	public static void main(String[] args) {
+		  String name;
+		     name= "Queens University Belfast Computer Science";  
+
+		     // TODO : output a sorted version of the sentence in ascending order by word to screen.   
+		     // eg for "QUEENS UNIVERSITY BELFAST COMPUTER SCIENCE", output "BELFAST COMPUTER QUEENS SCIENCE UNIVERSITY"  
+		     
+		     // Hint:
+		     // 1. Count the number of words in the String variable name 
+		     // (safe to assume name will only contain letters and spaces and will always start and end with letters)
+		     // 2. Arrays are easy to sort
+		     // Note: There is a function of the String class called .split() which would make this very easy. 
+		     // As a real challenge, see if you can do it without using this function and perhaps building your own 
+		     // version of it which takes a String and divides it into an array of words.
+	}
+
+}
---
+++ Week 08 File Reading/src/uk/ac/qub/filestuff/BookAnalysis.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package uk.ac.qub.filestuff;
+
+/**
+ * @author Gerald
+ *
+ */
+public class BookAnalysis {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 
+		// 1. Output the full text to screen
+		// 2. Output the number of lines 
+		// 3. Output the number of words 
+		// 4. Output the number of characters 
+		// 5. Output the number of times the main character's name (Yossarian) is referenced in the text.
+		// 6. Output the number of times the letter a is used. 
+		// 7. Redact the word Yossarian from the complete text and replace with ---------   
+		// (output the redacted text to an new file Catch22Redacted.txt.
+		// 8. Check if the redaction has been successful... check that Yossarian no longer appears in the new text.  
+
+	}
+
+}
