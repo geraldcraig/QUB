8d35417a8bc6e7691d50b558f2fc4d9482907632
Gerald Craig <geraldcraig@me.com>
2021-02-27 18:46:02 +0000

    week 17 added

---
+++ Week 15 Quiz Challenge/src/uk/ac/qub/quiz/Quiz.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.ac.qub.quiz;
+
+/**
+ * @author Gerald
+ *
+ */
+public class Quiz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Week 17 Exceptions/src/uk/ac/qub/exceptions/Exercise1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.ac.qub.exceptions;
+
+/**
+ * @author Gerald
+ *
+ */
+public class Exercise1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
