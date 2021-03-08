9d0f1b531e893f10668300b76421ecd412639aa0
Gerald Craig <geraldcraig@me.com>
2021-03-08 23:13:06 +0000

    week 22 updated

--- Week  22 Unit Testing/src/uk/ac/qub/music/mp3.java
+++ Week  22 Unit Testing/src/uk/ac/qub/music/mp3.java
@@ -2,50 +2,76 @@ package uk.ac.qub.music;
 
 public class mp3 {
 
-	public final static int LOWER_RATING=0; 
+	public final static int LOWER_RATING = 0;
 	
+	private int ref;
+	private String artist;
+	private String songName;
+	private int rating;
+	private int songLength;
+
 	public int getRef() {
 		return ref;
 	}
-	    
+
 	/**
 	 * If less than zero then throw exception
+	 * 
 	 * @param ref
 	 */
 	public void setRef(int ref) throws IllegalArgumentException {
-		
-		if (ref<=0){
-		this.ref = ref;
-		} throw new IllegalArgumentException("Ref less than 0");
+
+		if (ref <= 0) {
+			this.ref = ref;
+		}
+		throw new IllegalArgumentException("Ref less than 0");
 	}
-		public String getArtist() {
-			return artist;
+
+	public String getArtist() {
+		return artist;
 	}
-	public 	void setArtist(String artist) {
+
+	public void setArtist(String artist) {
 		this.artist = artist;
 	}
-	public 		String getSongName() {
-		return songName;}
+
+	public String getSongName() {
+		return songName;
+	}
+
 	public void setSongName(String songName) {
 		this.songName = songName;
 	}
-	public int getRating() 
-	{
+
+	public int getRating() {
 		return rating;
 	}
-	
-	
-public void setRating(int rating) {
-	if ((rating>1) && (rating<5)){	
-		this.rating = rating;
+
+	public void setRating(int rating) {
+		if ((rating > 1) && (rating < 5)) {
+			this.rating = rating;
+		}
+
 	}
 	
+	/**
+	 * @return the songLength
+	 */
+	public int getSongLength() {
+		return songLength;
 	}
-	
-	
-	
-	
-	
+
+	/**
+	 * @param songLength the songLength to set
+	 */
+	public void setSongLength(int songLength) {
+		if (songLength < 1 || songLength > 2500) {
+			System.err.println("Invalid song length");
+		} else {
+		this.songLength = songLength;
+		}
+	}
+
 	public mp3(int ref, String artist, String songName, int rating) {
 		this.ref = ref;
 		this.artist = artist;
@@ -53,19 +79,4 @@ public void setRating(int rating) {
 		this.rating = rating;
 	}
 
-	
-	
-
-
-
-
-	private int ref;
-	private String artist;
-	private String songName;
-	private int rating;
-	
-	
-	
-	
-	
 }
