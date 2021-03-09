fc12eaaa0ab43b9132a323035fdc81fc91cac862
Gerald Craig <geraldcraig@me.com>
2021-03-09 21:03:49 +0000

    exercises added

---
+++ CodingBat/Tests/warmup1/SleepInTest.java
@@ -0,0 +1,19 @@
+package warmup1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SleepInTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
