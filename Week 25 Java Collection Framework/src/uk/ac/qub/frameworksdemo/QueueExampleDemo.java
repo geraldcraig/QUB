c7711780892213548c94a9cd771b6934a86d7a50
Gerald Craig <geraldcraig@me.com>
2021-03-21 17:44:10 +0000

    week 25

--- Week 25 Java Collection Framework/src/uk/ac/qub/frameworksdemo/QueueExampleDemo.java
+++ Week 25 Java Collection Framework/src/uk/ac/qub/frameworksdemo/QueueExampleDemo.java
@@ -1,7 +1,11 @@
 package uk.ac.qub.frameworksdemo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class QueueExampleDemo {
 
@@ -9,18 +13,45 @@ public class QueueExampleDemo {
 		 
 		int time = 10;
 		
-		Queue<Integer> queue = new LinkedList<Integer>();
+		ArrayList<String> names = new ArrayList<>();
+		names.add("Matthew");
+		names.add("Aidan");
+		names.add("Neil");
+		names.add("Olga");
+		names.add("Janak");
+		names.add("John");
+				
+		Queue<Integer> queueNum = new LinkedList<Integer>();
 		
 		for (int i = time; i >= 0; i--) {
-			queue.add(i);
+			queueNum.add(i);
 		}
 		
-		System.out.println("Queue items: " + queue.toString());
+		Queue<String> queue = new LinkedList<String>(names);
+		Queue<String> pQueue = new PriorityQueue<String>(names);
+		
+		System.out.println("Queue items: " + queueNum.toString());
+		
+		while (!queueNum.isEmpty()) {
+			System.out.println(queueNum.remove());
+			System.out.println("Queue items: " + queueNum.toString());
+			Thread.sleep(1000);
+		}
+		
+		System.out.println("Queue items: " + queueNum.toString());
 		
 		while (!queue.isEmpty()) {
 			System.out.println(queue.remove());
 			System.out.println("Queue items: " + queue.toString());
-			Thread.sleep(1000);
+			Thread.sleep(2000);
+		}
+		
+		System.out.println("Queue items: " + pQueue.toString());
+		
+		while (!pQueue.isEmpty()) {
+			System.out.println(pQueue.remove());
+			System.out.println("Queue items: " + pQueue.toString());
+			Thread.sleep(2000);
 		}
 	}
 
