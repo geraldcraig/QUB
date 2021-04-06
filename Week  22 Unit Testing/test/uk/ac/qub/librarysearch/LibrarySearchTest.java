3f33d850123adaec585d231d10ba6b3b59e53e13
Gerald Craig <geraldcraig@me.com>
2021-04-06 22:23:36 +0100

    week 22 updated

--- Week  22 Unit Testing/test/uk/ac/qub/librarysearch/LibrarySearchTest.java
+++ Week  22 Unit Testing/test/uk/ac/qub/librarysearch/LibrarySearchTest.java
@@ -5,25 +5,52 @@ package uk.ac.qub.librarysearch;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import uk.ac.qub.librarysearch.solution.Book;
+import uk.ac.qub.librarysearch.solution.LibrarySearch;
 
 /**
  * @author Gerald
  *
  */
 class LibrarySearchTest {
+	Book bk1, bk2;
+	String author, title, ISBN;
+	int rating;
+	
+	ArrayList<Book> arrayList;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
+		author = "author";
+		title = "title";
+		ISBN = "0123456789";
+		rating = 3;
+		
+		bk1 = new Book(ISBN, author, title, rating);
+		
+		arrayList = new ArrayList<Book>();
+		
+		arrayList.add(bk1);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testSearchByTitle() {
+
+		ArrayList<Book> results =  LibrarySearch.searchByTitle(arrayList, title);
+		
+		if (results.contains(bk1)){
+			assertTrue(true);
+		} else {
+			assertTrue(false);
+		}
 	}
 
 }
