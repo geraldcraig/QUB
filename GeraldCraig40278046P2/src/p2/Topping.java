33d1381970c050b87d06954cd88d4920fe27626b
Gerald Craig <geraldcraig@me.com>
2021-04-22 20:29:28 +0100

    updated

---
+++ GeraldCraig40278046P2/src/p2/Topping.java
@@ -0,0 +1,21 @@
+package p2;
+
+/**
+ * Topping enum
+ * @author geraldcraig
+ *
+ */
+public enum Topping {
+	
+	EXTRACHEESE,
+	PEPPERONI,
+	BEEF,
+	CHICKEN,
+	HAM,
+	PINEAPPLE,
+	PEPPERS,
+	ONIONS,
+	MUSHROOMS,
+	OLIVES
+
+}
