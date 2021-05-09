681c8a54d4f24253b123c1de33918b359cf30d68
Gerald Craig <geraldcraig@me.com>
2021-05-09 10:40:38 +0100

    my projects updated

---
+++ My Projects/Test/music/collection/SevenInchTest.java
@@ -0,0 +1,19 @@
+package music.collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class SevenInchTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
