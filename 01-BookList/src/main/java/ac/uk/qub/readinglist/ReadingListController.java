f0a180081532809f3f515bbfd53218fab3af944f
Gerald Craig <geraldcraig@me.com>
2021-07-12 09:38:15 +0100

    booklist updated

--- 01-BookList/src/main/java/ac/uk/qub/readinglist/ReadingListController.java
+++ 01-BookList/src/main/java/ac/uk/qub/readinglist/ReadingListController.java
@@ -16,18 +16,14 @@ public class ReadingListController {
   private ReadingListRepository readingListRepository;
 
   @Autowired
-  public ReadingListController(
-             ReadingListRepository readingListRepository) {
+  public ReadingListController(ReadingListRepository readingListRepository) {
     this.readingListRepository = readingListRepository;
      }
 
   @RequestMapping(value="/{reader}", method=RequestMethod.GET)
-  public String readersBooks(
-      @PathVariable("reader") String reader,
-      Model model) {
+  public String readersBooks(@PathVariable("reader") String reader, Model model) {
 
-    List<Book> readingList =
-        readingListRepository.findByReader(reader);
+    List<Book> readingList = readingListRepository.findByReader(reader);
     if (readingList != null) {
       model.addAttribute("books", readingList);
     }
@@ -35,8 +31,7 @@ public class ReadingListController {
   }
 
   @RequestMapping(value="/{reader}", method=RequestMethod.POST)
-  public String addToReadingList(
-            @PathVariable("reader") String reader, Book book) {
+  public String addToReadingList(@PathVariable("reader") String reader, Book book) {
     book.setReader(reader);
     readingListRepository.save(book);
     return "redirect:/{reader}";
