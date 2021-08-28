7e12c29fb97eae4cede8df50005828827a57da40
geraldcraig <geraldcraig@me.com>
2021-08-28 15:51:31 +0100

    updated

---
+++ 01-CarDatabase/src/main/java/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,29 @@
+package cardatabase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import cardatabase.domain.Car;
+import cardatabase.domain.CarRepository;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+	@Autowired
+	private CarRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+	
+	@Bean
+	
+	CommandLineRunner runner() {
+		return args -> {
+			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000));
+			
+		};
+	}
+}
