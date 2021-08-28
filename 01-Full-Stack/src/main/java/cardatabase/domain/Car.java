d64b7355603caef297dd5639df6ef91fc6ee24fc
Gerald Craig <geraldcraig@me.com>
2021-08-28 07:45:00 +0100

    full stack updated

--- 01-Full-Stack/src/main/java/cardatabase/domain/Car.java
+++ 01-Full-Stack/src/main/java/cardatabase/domain/Car.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.FetchType;
 
 @Entity
 public class Car {
@@ -13,11 +16,13 @@ public class Car {
 	private String brand, model, color, registerNumber;
 	private int year, price;
 	
-	public Car() {
-		
-	}
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name = "owner")
+	private Owner owner;
 	
-	public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+	public Car() {}
+	
+	public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
 		super();
 		this.brand = brand;
 		this.model = model;
@@ -25,66 +30,53 @@ public class Car {
 		this.registerNumber = registerNumber;
 		this.year = year;
 		this.price = price;
+		this.owner = owner;
+	}
+	
+	public Owner getOwner() {
+		return owner;
 	}
 
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
 
 	public String getBrand() {
 		return brand;
 	}
-
-
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
-
-
 	public String getModel() {
 		return model;
 	}
-
-
 	public void setModel(String model) {
 		this.model = model;
 	}
-
-
 	public String getColor() {
 		return color;
 	}
-
-
 	public void setColor(String color) {
 		this.color = color;
 	}
-
-
 	public String getRegisterNumber() {
 		return registerNumber;
 	}
-
-
 	public void setRegisterNumber(String registerNumber) {
 		this.registerNumber = registerNumber;
 	}
-
-
 	public int getYear() {
 		return year;
 	}
-
-
 	public void setYear(int year) {
 		this.year = year;
 	}
-
-
 	public int getPrice() {
 		return price;
 	}
-
-
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	
 	
 }
