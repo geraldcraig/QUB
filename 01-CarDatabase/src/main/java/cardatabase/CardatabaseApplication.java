84f7e45eb2c94ebf293a5556727156edee501bae
geraldcraig <geraldcraig@me.com>
2021-09-14 23:01:58 +0100

    cardatabase updated

--- 01-CarDatabase/src/main/java/cardatabase/CardatabaseApplication.java
+++ 01-CarDatabase/src/main/java/cardatabase/CardatabaseApplication.java
@@ -6,12 +6,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.sun.jmx.mbeanserver.Repository;
+
 import cardatabase.domain.Car;
 import cardatabase.domain.CarRepository;
+import cardatabase.domain.Owner;
+import cardatabase.domain.OwnerRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
 	@Autowired
+	private OwnerRepository orepository;
 	private CarRepository repository;
 
 	public static void main(String[] args) {
@@ -21,8 +26,17 @@ public class CardatabaseApplication {
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000));
+			Owner owner1 = new Owner("John" , "Johnson");
+			Owner owner2 = new Owner("Mary" , "Robinson");
+			//orepository.save(owner1);
+			//orepository.save(owner2);
 			
+			Car car = new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1);
+			repository.save(car);
+			car = new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2);
+			repository.save(car);
+			car = new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2);	
+			repository.save(car);
 		};
 	}
 }
