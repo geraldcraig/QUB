ba44833061232d6667516517fb6e1360a4d6ef7a
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-04-12 21:33:01 +0100

    refresher updated

--- Java Refresher/src/uk/ac/qub/three/quiz/Quiz.java
+++ Java Refresher/src/uk/ac/qub/three/quiz/Quiz.java
@@ -1,10 +1,52 @@
 package uk.ac.qub.three.quiz;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Quiz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// declaring the questions and answers
+		String [] jokeQuestions = {"Largest planet?", "Worse song ever?", "Capital of Peru?", "Roman god of War?"};
+		String [] jokeAnswers = {"Jupiter", "Castles in the Sky", "Lima", "Mars"};
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		Random random = new Random();
+		String answer;
+		String response;
+		
+		System.out.println("General Knowledge quiz...");
+		
+		do {
+		
+		int number = random.nextInt(4);
+		System.out.println(jokeQuestions[number]);
+		
+		answer = scanner.next();
+		
+		if (answer.equalsIgnoreCase(jokeAnswers[number])) {
+			System.out.println("Well done !");
+		} else {
+			System.out.println("Not a bad answer but it was " + jokeAnswers[number]);
+		}
+		
+		System.out.println("Another question (Y or N)..");
+		
+		response = scanner.next();
+		
+		}
+		
+		while (response.equalsIgnoreCase("Y")); {
+			
+			 
+			System.out.println("Thanks for playing");
+		
+		scanner.close();
 
+	}
+	
 	}
 
 }
--- Java Refresher/src/uk/ac/qub/two/fizzbuzz/FizzBuzz.java
+++ Java Refresher/src/uk/ac/qub/two/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,18 @@ package uk.ac.qub.two.fizzbuzz;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for (int i = 1; i <= 100; i++) {
+			if (i % 15 == 0) {
+				System.out.println("Fizz Buzz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else {
+				System.out.println(i);
+			}
+		}
 
 	}
 
