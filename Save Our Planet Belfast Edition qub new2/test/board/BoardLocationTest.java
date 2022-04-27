f2fa053d8d7c1ba87d9b27905446f260310c90c3
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-04-27 21:08:10 +0100

    updated

---
+++ Save Our Planet Belfast Edition qub new2/test/board/BoardLocationTest.java
@@ -0,0 +1,31 @@
+package board;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BoardLocationTest {
+	
+	Areas boardlocation = new Areas(5, "Water Works", "North Belfast", 100, 30, .10, 60, .03, 120, .12);
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+
+
+	@Test
+	void testGetIndex() {
+		
+		assertEquals(5, boardlocation.getIndex());
+	}
+
+	@Test
+	void testGetAreaName() {
+		
+		assertEquals("Water Works", boardlocation.getAreaName());
+	}
+
+
+}
